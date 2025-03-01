ef28af4b585274c258680ce06307c26051ba6bae
tom-256 <tom-256@users.noreply.github.com>
2025-03-01 16:14:00 +0900

    Fix code style issues with RuboCop

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,13 +1,13 @@
 class Task < ApplicationRecord
   validates :title, presence: true
-  validates :status, presence: true, inclusion: { in: ['todo', 'in_progress', 'done'] }
-  
+  validates :status, presence: true, inclusion: { in: [ "todo", "in_progress", "done" ] }
+
   # ステータスのデフォルト値を設定
   after_initialize :set_default_status, if: :new_record?
-  
+
   private
-  
+
   def set_default_status
-    self.status ||= 'todo'
+    self.status ||= "todo"
   end
 end
